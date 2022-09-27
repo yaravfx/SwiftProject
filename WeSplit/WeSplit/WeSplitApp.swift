e6a0f7ea26e6c571a3a7aa5a16e5b67ee74e8396
Yara <yaravfx@gmail.com>
2022-09-27 00:02:55 -0700

    initial commit of WeSplit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Yara on 2022-09-25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
